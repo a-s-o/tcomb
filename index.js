d9a81e59767eb48d0876a493664afe71928ff2fd
Amandeep <a-s-o@users.noreply.github.com>
2015-10-24 23:18:02 -0400

    Added test.not method

--- index.js
+++ index.js
@@ -4,22 +4,36 @@ const t = require('tcomb-validation');
 
 const Type = t.irreducible('Type', t.isType);
 
+function typedFunc (obj) {
+   return t.func(obj.inputs || [], obj.outputs || t.Any).of(obj.fn);
+}
+
+// Testing helpers
+
 function test (value, type) {
    const result = t.validate(value, type);
    if (!result.isValid()) t.fail(result.firstError().message);
 }
 
-function typedFunc (obj) {
-   return t.func(obj.inputs || [], obj.outputs || t.Any).of(obj.fn);
+function testNot (value, type) {
+   const result = t.validate(value, type);
+   if (result.isValid()) t.fail(`Expected value not to be ${t.getTypeName(type)}`);
 }
+
+const testFunc = typedFunc({
+   inputs: [t.Any, Type],
+   fn: test
+});
+
+testFunc.not = typedFunc({
+   inputs: [t.Any, Type],
+   fn: testNot
+});
 
 module.exports = t.mixin(t, {
    Type,
 
-   test: typedFunc({
-      inputs: [t.Any, Type],
-      fn: test
-   }),
+   test: testFunc,
    typedFunc: typedFunc({
       inputs: [t.struct({
          inputs: t.maybe(t.Array),
